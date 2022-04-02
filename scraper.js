d86d9ad378d95961ae3fc173d831ec96acb1beff
Kishore Kumar <kishore1234500000@gmail.com>
2022-04-02 13:13:29 +0530

    add duration and imageUrl in getById function

--- scraper.js
+++ scraper.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import cheerio from "cheerio";
 const BASE_URL = "https://www.imdb.com";
+const CUSTOM_HEADERS = {
+  "User-Agent" : "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0",
+}
 
 const getById = async (title_id) => {
   const url = `${BASE_URL}/title/${title_id}`;
@@ -8,19 +11,23 @@ const getById = async (title_id) => {
   await axios({
     method: 'get',
     url: url,
-    headers: {
-      "User-Agent" : "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"
-    }
+    headers: CUSTOM_HEADERS,
   })
     .then(response => response.data.toString())
     .then((HTML) => {
       const $ = cheerio.load(HTML);
       const title = $(".sc-94726ce4-1.iNShGo > h1").text();
       const year = $(".sc-94726ce4-1.iNShGo > div > ul > li:nth-child(1) > span").text();
-      const rating = $(".sc-7ab21ed2-2.kYEdvH > span").text();
+      const duration = $("div.sc-94726ce4-1.iNShGo > div > ul > li:nth-child(3)").text() || $("div.sc-94726ce4-1.iNShGo > div > ul > li:nth-child(2)").text();
+      const rating = $(".sc-1201338c-0.ljHBFo > span:nth-child(2)").text();
+      const imageViewer = $("a[aria-label='View {Title} Poster']").attr('href');
       result.title = title;
-      result.year = year;
+      result.year = year || "N/A";
+      result.duration = duration;
+      result.rating = rating || "N/A";
+      result.id = title_id;
       result.url = url;
+      result.imageViewer = imageViewer?`${BASE_URL}${imageViewer}`:"N/A";
     })
     .catch(() => result.error = "Error occurred");
   return result;
@@ -32,18 +39,15 @@ const getTop250 = async () => {
   await axios({
     method: 'get',
     url: url,
-    headers: {
-      "User-Agent" : "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"
-    }
+    headers:  CUSTOM_HEADERS,
   })
     .then(response => response.data.toString())
     .then((HTML) => {
       const $ = cheerio.load(HTML);
       let list = $(".lister-list > tr");
-      // console.log(list);
       list.each(function (idx) {
         result.movies.push({
-          movieOrder: idx + 1,
+          rank: idx + 1,
           movieId: $(this).find("td:nth-child(4) > div").data("titleid"),
           Title: $(this).find(".titleColumn > a").text(),
           Year: $(this).find(".titleColumn > span").text(),
@@ -62,9 +66,7 @@ const getToptv250 = async () => {
   await axios({
     method: 'get',
     url: url,
-    headers: {
-      "User-Agent" : "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"
-    }
+    headers: CUSTOM_HEADERS,
   })
     .then(response => response.data.toString())
     .then((HTML) => {
@@ -73,7 +75,7 @@ const getToptv250 = async () => {
       // console.log(list);
       list.each(function (idx) {
         result.tvShows.push({
-          tvShowOrder: idx + 1,
+          rank: idx + 1,
           tvShowId: $(this).find("td:nth-child(4) > div").data("titleid"),
           Title: $(this).find(".titleColumn > a").text(),
           Year: $(this).find(".titleColumn > span").text(),
