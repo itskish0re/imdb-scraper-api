64b016056ce387ca2315c73aa8e2bbcf26626047
Kishore Kumar <kishore1234500000@gmail.com>
2022-05-02 13:03:04 +0530

    some bugfix and improvements

--- server.js
+++ server.js
@@ -1,42 +1,42 @@
 import express from "express";
-import "./scrapers/scraper.js";
+import * as S from "./scrapers/scraper.js";
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.get("/title/:titleId", (req, res) => {
-  getTitleById(req.params.titleId)
+  S.getTitleById(req.params.titleId)
     .then((response) => res.json(response));
 });
 
 app.get("/cast/:nameId", (req, res) => {
-  getNameById(req.params.nameId)
+  S.getNameById(req.params.nameId)
     .then((response) => res.json(response));
 });
 
 app.get("/top250", (req, res) => {
-  getTop250()
+  S.getTop250()
     .then((response) => res.json(response));
 });
 
 app.get("/toptv250", (req, res) => {
-  getToptv250()
+  S.getToptv250()
     .then((response) => res.json(response));
 });
 
 app.get("/search/title/:keyWord&:exact", (req, res) => {
-  searchByTitle(req.params.keyWord, req.params.exact)
+  S.searchByTitle(req.params.keyWord, req.params.exact)
     .then((response) => res.json(response));
 });
 
 app.get("/search/cast/:keyWord&:exact", (req, res) => {
-  searchByName(req.params.keyWord, req.params.exact)
+  S.searchByName(req.params.keyWord, req.params.exact)
     .then((response) => res.json(response));
 });
 
 app.get("/title/bulk/:titleIds", (req, res) => {
   if(req.params.titleIds.split(",").length <= 10) {
-    bulkTitleById(req.params.titleIds)
+    S.bulkTitleById(req.params.titleIds)
       .then((response) => res.json(response));
   }else {
     res.json({
