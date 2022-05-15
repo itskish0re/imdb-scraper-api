e3c6d60df13e107099cd0e9bef34d401ae655053
Kishore Kumar <kishore1234500000@gmail.com>
2022-05-15 17:45:55 +0530

    some indentation changes

--- server.js
+++ server.js
@@ -11,39 +11,32 @@ app.get("/", (req, res) => {
 });
 
 app.get("/title/:titleId", (req, res) => {
-  S.getTitleById(req.params.titleId)
-    .then((response) => res.json(response));
+  S.getTitleById(req.params.titleId).then((response) => res.json(response));
 });
 
 app.get("/cast/:nameId", (req, res) => {
-  S.getNameById(req.params.nameId)
-    .then((response) => res.json(response));
+  S.getNameById(req.params.nameId).then((response) => res.json(response));
 });
 
 app.get("/top250", (req, res) => {
-  S.getTop250()
-    .then((response) => res.json(response));
+  S.getTop250().then((response) => res.json(response));
 });
 
 app.get("/toptv250", (req, res) => {
-  S.getToptv250()
-    .then((response) => res.json(response));
+  S.getToptv250().then((response) => res.json(response));
 });
 
 app.get("/search/title/:keyWord/:exact", (req, res) => {
-  S.searchByTitle(req.params.keyWord, req.params.exact)
-    .then((response) => res.json(response));
+  S.searchByTitle(req.params.keyWord, req.params.exact).then((response) => res.json(response));
 });
 
 app.get("/search/cast/:keyWord/:exact", (req, res) => {
-  S.searchByName(req.params.keyWord, req.params.exact)
-    .then((response) => res.json(response));
+  S.searchByName(req.params.keyWord, req.params.exact).then((response) => res.json(response));
 });
 
 app.get("/title/bulk/:titleIds", (req, res) => {
   if(req.params.titleIds.split(",").length <= 10) {
-    S.bulkTitleById(req.params.titleIds)
-      .then((response) => res.json(response));
+    S.bulkTitleById(req.params.titleIds).then((response) => res.json(response));
   }else {
     res.json({
       "Error": "titleids search limit is up to ten only!",
@@ -53,8 +46,7 @@ app.get("/title/bulk/:titleIds", (req, res) => {
 
 app.get("/cast/bulk/:nameIds", (req, res) => {
   if(req.params.nameIds.split(",").length <= 10) {
-    S.bulkNameById(req.params.nameIds)
-      .then((response) => res.json(response));
+    S.bulkNameById(req.params.nameIds).then((response) => res.json(response));
   }else {
     res.json({
       "Error": "nameids search limit is up to ten only!",
@@ -67,3 +59,4 @@ app.get("*", (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`server running on http://localhost:${PORT}`));
+
